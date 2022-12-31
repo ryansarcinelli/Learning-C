1b2d95160a22a2f7ac7716e9bcc93a4c543143d9
ryansarcinelli <114365610+ryansarcinelli@users.noreply.github.com>
2022-12-31 03:13:24 -0300

    deleting some executables

--- homeworks2/hom5.c
+++ homeworks2/hom5.c
@@ -7,5 +7,59 @@ colocado num vetor resultante VR e mostrado ao final do programa.*/
 
 int main()
 {
+    const int x=2;
+    int a[x], b[x],op[x], soma=0;
+    char operacao;
+
+    for(int i=1;i<=x;i++)
+    {
+        printf("Digite o %d numero do vetor a:\n", i);
+        scanf("%d", &a[i]);
+        printf("Digite o %d numero do vetor b:\n", i);
+        scanf("%d", &b[i]);
+    }
+
+    printf("Diga a sua operacao (+, -, *, /)\n");
+    scanf("%s", &operacao);
+
+    switch (operacao)
+    {
+        case '+':
+        for(int i=1;i<=x;i++)
+        {
+            op[i]=(a[i])+(b[i]);
+        }
+        break;
+
+        case '-':
+        for(int i=1;i<=x;i++)
+        {
+            op[i]=(a[i])-(b[i]);
+        }
+        break;
+
+        case '*':
+        for(int i=1;i<=x;i++)
+        {
+            op[i]=(a[i])*(b[i]);
+        }
+        break;
+
+        case '/':
+        for(int i=1;i<=x;i++)
+        {
+            op[i]=(a[i])/(b[i]);
+        }
+        break;
+    }
+    
+    for(int i=1;i<=x;i++)
+    {
+        printf("%d ", op[i]);
+        soma = soma + op[i];
+    }
+    
+    printf("\nSoma e e %d", soma);
+
     return 0;
 }
